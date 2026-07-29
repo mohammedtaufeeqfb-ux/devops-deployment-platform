f6592967a5f3dab4238d2b597bf9935722f1b955
Mohammed Taufeeq <mohammedtaufeeqfb@gmail.com>
2026-07-29 20:18:17 +0530

    updated main.tf (ami id)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_server" {
-  ami           = "ami-0f58b397bc5c1f2e8"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
   tags = merge(
