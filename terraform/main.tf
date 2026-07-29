d4379017c397af45aca96b56695b03234668830e
Mohammed Taufeeq <mohammedtaufeeqfb@gmail.com>
2026-07-29 19:47:59 +0530

    refactor(terraform): add production folder structure and variables

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "web_server" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "project-phoenix-web"
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,7 @@
+aws_region   = "ap-south-1"
+
+project_name = "project-phoenix"
+
+environment  = "dev"
+
+instance_type = "t3.micro"
