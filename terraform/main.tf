577e8ce357492272cb4c1a0a2a1c53226c0ba78d
Mohammed Taufeeq <mohammedtaufeeqfb@gmail.com>
2026-07-29 20:06:48 +0530

    updated local.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,10 @@ resource "aws_instance" "web_server" {
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = var.instance_type
 
-  tags = {
-    Name = "project-phoenix-web"
+  tags = merge(
+  local.common_tags,
+  {
+    Name = local.common_name
   }
+ )
 }
